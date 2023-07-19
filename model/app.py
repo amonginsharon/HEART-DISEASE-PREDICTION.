087541e2a73d7b8a8dedde69e4dfdeeb17b7931d
amonginsharon <139625225+amonginsharon@users.noreply.github.com>
2023-07-19 03:49:42 -0700

    Update app.py

--- model/app.py
+++ model/app.py
@@ -3,20 +3,22 @@ import pandas as pd
 import pickle
 
 def main():
+    set_page_style()
+
     st.title("Heart Disease Prediction")
     # User input fields
-    age = st.number_input("age", value=0, min_value=0, max_value=150, step=1)
-    gender = st.selectbox("gender", options=["Female", "Male"])
-    height = st.number_input("height (cm)", value=0.0, min_value=0.0, step=1.0)
-    weight = st.number_input("weight (kg)", value=0.0, min_value=0.0, step=0.1)
-    ap_hi = st.number_input("systolic Blood Pressure", value=0, min_value=0, max_value=300, step=1)
-    ap_lo = st.number_input("diastolic Blood Pressure", value=0, min_value=0, max_value=300, step=1)
-    cholesterol = st.selectbox("cholesterol", options=["Normal", "Above Normal", "Well Above Normal"])
-    gluc = st.selectbox("gluc", options=["Normal", "Above Normal", "Well Above Normal"])
+    age = st.number_input("Age", value=0, min_value=0, max_value=150, step=1)
+    gender = st.selectbox("Gender", options=["Female", "Male"])
+    height = st.number_input("Height (cm)", value=0.0, min_value=0.0, step=1.0)
+    weight = st.number_input("Weight (kg)", value=0.0, min_value=0.0, step=0.1)
+    ap_hi = st.number_input("Systolic Blood Pressure", value=0, min_value=0, max_value=300, step=1)
+    ap_lo = st.number_input("Diastolic Blood Pressure", value=0, min_value=0, max_value=300, step=1)
+    cholesterol = st.selectbox("Cholesterol", options=["Normal", "Above Normal", "Well Above Normal"])
+    gluc = st.selectbox("Glucose", options=["Normal", "Above Normal", "Well Above Normal"])
     smoke = st.selectbox("Smoking", options=["No", "Yes"])
-    alco = st.selectbox("alco Consumption", options=["No", "Yes"])
-    active = st.selectbox("active", options=["No", "Yes"])
-    
+    alco = st.selectbox("Alcohol Consumption", options=["No", "Yes"])
+    active = st.selectbox("Physical Activity", options=["No", "Yes"])
+
     # Convert categorical inputs to numeric
     gender = 1 if gender == "Male" else 0
     cholesterol = convert_label(cholesterol)
@@ -24,7 +26,7 @@ def main():
     smoke = 1 if smoke == "Yes" else 0
     alco = 1 if alco == "Yes" else 0
     active = 1 if active == "Yes" else 0
-    
+
     # Create feature DataFrame
     data = pd.DataFrame({
         "age": [age],
@@ -39,33 +41,56 @@ def main():
         "alco": [alco],
         "active": [active]
     })
-    
+
     # Load the trained model
     model = load_model()
-    
+
     # Make prediction
     if st.button("Predict"):
         result = predict(model, data)
         st.write("Prediction:", result)
 
+def set_page_style():
+    # Set page style
+    page_bg_color = "#0074D9"  # Blue color (Hex: #0074D9)
+    text_color = "#FFFFFF"  # White color
+    st.markdown(
+        f"""
+        <style>
+        body {{
+            background-color: {page_bg_color};
+            color: {text_color};
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
 def load_model():
     # Load the pre-trained model
-    with open("model_pickle (2).pkl", "rb") as file:
+    with open("model_pickle.pkl", "rb") as file:
         model = pickle.load(file)
     return model
 
 def predict(model, data):
     # Perform prediction using the loaded model
     result = model.predict(data)
-    return result[0]
+
+    # Convert the numeric result back to descriptive label
+    if result == 0:
+        return "Great! You are fit. It's time to celebrate."
+    elif result == 1:
+        return "Oops! You are most likely to have a disease."
+    elif result == 2:
+        return "Oops! You are most likely to have a disease."
 
 def convert_label(label):
     if label == "Normal":
-        return "great you are fit, it is time to celebrate"
+        return 0
     elif label == "Above Normal":
-        return "oops you are most likely to be having a disease"
+        return 1
     elif label == "Well Above Normal":
-        return "oops you are most likely to have a disease"
+        return 2
 
 if __name__ == "__main__":
     main()
